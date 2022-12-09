ba67fdfb5bb04ebb926fe750c0df90b02949da72
kenziemanningg <116291046+kenziem07@users.noreply.github.com>
2022-12-09 16:53:47 +0000

    Added script code

--- script.js
+++ script.js
@@ -1,16 +1,159 @@
 // example function call to display a dialog box
-// let's connect out the code below, which is top-level code
+
+// let's comment out the code below, which is top-level code
+
 // that automatically runs when the script is loaded
-// alert("welcome, this isn't is a javascript popup alert");
+
+//alert("welcome, this isn't is a javascript popup alert");
+
+ 
 
 function submitForm(name, hometown) {
-  // search the DOM for the specific tag with the id "guestlist"
+
+  // search the DOM for a specific tag with the id "guestlist"
+
   var guestlist = document.getElementById("guestlist");
+
   var n = document.getElementById("name");
-  var h = document.getElementById("hometown"); 
-  guestlist.innerHTML += "<li>" + name + ". " + hometown + "</li"; 
+
+  var h = document.getElementById("hometown");
+
+  //saveLocal(name, hometown);
+
+  saveRemote(name, hometown);
+
+  guestlist.innerHTML += "<li>" + name + ". " + hometown + "</li>";
+
   n.value = "";
-  h.value = ""; 
+
+  h.value = "";
+
   n.focus();
-  return false();
-} 
+
+  return false;
+
+}
+
+ 
+
+function saveLocal(name, hometown) {
+
+  var currentguestlist = localStorage.getItem("guestlist");
+
+  localStorage.setItem("guestlist", currentguestlist + "<li>" + name + ". " + hometown + "</li>"); 
+
+}
+
+ 
+
+function saveRemote(name, hometown) {
+
+  $.get("save.php",{"name":name,"hometown":hometown});
+
+}
+
+ 
+
+function loadRemote() {
+
+  $.get("load.php",{},function(data) {
+
+    data = JSON.parse(data);
+
+    console.log(data);
+
+    console.log("row by row:");
+
+    var html = "";
+
+    for (var i in data) {
+
+      console.log(data[i]);
+
+      if (data[i])
+
+        html += "<li>" + data[i] + "</li>";
+
+    }
+
+    $("#guestlist").html(html);
+
+  });
+
+}
+
+ 
+
+ 
+
+ 
+
+function clearStorageGB() {
+
+  //var guestlist = document.getElementById("guestlist");
+
+  //guestlist.innerHTML = "";
+
+  $("#guestlist").html("The guest list has been cleared.");
+
+  localStorage.setItem("guestlist","");
+
+}
+
+ 
+
+function checkStorageGB() {
+
+  var guestlist = document.getElementById("guestlist");
+
+  var currentguestlist = localStorage.getItem("guestlist");
+
+  guestlist.innerHTML = currentguestlist;
+
+}
+
+ 
+
+function clearStorage() {
+
+  var answerlist = document.getElementById("answerlist");
+
+  answerlist.innerHTML = "";
+
+  localStorage.setItem("answerlist","");
+
+}
+
+ 
+
+function checkStorage() {
+
+  var answerlist = document.getElementById("answerlist");
+
+  var currentanswers = localStorage.getItem("answerlist");
+
+  answerlist.innerHTML = currentanswers;
+
+}
+
+ 
+
+function submitAnswer(answer) {
+
+  // search the DOM for a specific tag with the id "answerlist"
+
+  var answerlist = document.getElementById("answerlist");
+
+  var currentanswers = localStorage.getItem("answerlist");
+
+  localStorage.setItem("answerlist", currentanswers + "<br />" + answer.value);
+
+  answerlist.innerHTML += "<br />" + answer.value;
+
+  answer.value = "";
+
+  answer.focus();
+
+  return false;
+
+}
